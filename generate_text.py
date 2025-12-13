b2c9b4f550f4993d681cfd591aa2ac997b61d0e9
supraja777 <ssupraja777@gmail.com>
2025-12-13 14:46:13 -0500

    Added logic for temperature and topK

--- generate_text.py
+++ generate_text.py
@@ -3,15 +3,30 @@ import tiktoken
 from gpt_model import GPTModel
 from gpt_config import GPT_CONFIG_124M
 
-def generate_text_simple(model, idx, max_new_tokens, context_size):
+def generate_text_simple(model, idx, max_new_tokens, context_size, temperature = 0.0, top_K = None, eos_id = None):
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         
         logits = logits[:, -1, :]
-        probas = torch.softmax(logits, dim = -1)
-        idx_next = torch.argmax(probas, dim = -1, keepdim=True)
+        if top_k is not None:
+            top_logits, _ = torch.topK(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensot(float('-inf')).to(logits.device),
+                logits
+            )
+
+            if temperature > 0.0:
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim = -1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim = -1, keepdim = True)
+        if idx_next == eos_id:
+            break
         idx = torch.cat((idx, idx_next), dim = 1)
 
     return idx
